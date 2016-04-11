bf67204e3d85b228ad81a1bf55c825b912982d5e
Misigno <efournier92@gmail.com>
2016-04-11 15:05:22 -0400

    updates README

--- config.ru
+++ config.ru
@@ -1,2 +1,7 @@
-require ::File.expand_path('../config/environment', __FILE__)
+require './server'
+
+$stdout.sync = true
+
 run Rails.application
+
+require ::File.expand_path('../config/environment', __FILE__)
---
+++ spec/features/users/user_logs_in_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+# USER STORY:
+# As an unauthenticated User
+# I want to log in
+# So I can post APIs & reviews
+
+feature 'user signs in' do
+  let(:user1) { FactoryGirl.create(:user) }
+  scenario 'provides valid credentials, successfully signs in' do
+    visit root_path
+
+    click_link 'Sign In'
+    fill_in 'Email', with: user1.email
+    fill_in 'Password', with: user1.password
+
+    click_button 'Sign In'
+    expect(page).to have_content('Welcome Back!')
+    expect(page).to have_content('Sign Out')
+  end
+
+  scenario 'provides invalid credentials, sees error message' do
+    visit root_path
+
+    click_link 'Sign In'
+    fill_in 'Email', with: 'something@some.com'
+    fill_in 'Password', with: 'password'
+
+    click_button 'Sign In'
+    expect(page).to_not have_content('Welcome Back')
+    expect(page).to_not have_content('Sign Out')
+    expect(page).to have_content('Invalid email or password.')
+  end
+
+  scenario 'already signed in user is unable to sign in again' do
+    visit new_user_session_path
+    fill_in 'Email', with: user1.email
+    fill_in 'Password', with: user1.password
+
+    click_button 'Sign In'
+    expect(page).to have_content('Sign Out')
+    expect(page).to_not have_content('Sign In')
+
+    visit new_user_session_path
+    expect(page).to have_content('You are already signed in.')
+  end
+end
