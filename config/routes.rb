96ba5a0e63dc13bccb8b53578ded53544838c99f
Matt T <MMT@Matts-MacBook-Pro.local>
2016-04-13 16:53:38 -0400

    refactoring ajax upvote/downvotes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index]
   devise_for :users, controllers: { registrations: 'registrations' }
+
+  scope module: 'api' do
+    namespace :v1 do
+      resources :votes
+      get '/upvote/' => 'votes#upvote'
+      get '/downvote/' => 'votes#downvote'
+    end
+  end
 end
