24bd2d04205118c74afd1a29251fefc88c760890
Misigno <efournier92@gmail.com>
2016-04-14 13:31:15 -0400

    user page shows user reviews & apis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user         = User.find(params[:id])
+    @user_apis    = Api.where(user_id: current_user)
+    @user_reviews = Review.where(user_id: current_user)
   end
+
 end
