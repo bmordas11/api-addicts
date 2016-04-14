86dd1a242cfb44df520acb456b84f977dab93b7a
bmordas11 <brett.mordas@gmail.com>
2016-04-14 10:36:32 -0400

    Fixed hound errors on user model and review_mailer

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,9 +1,9 @@
 class ReviewMailer < ApplicationMailer
   def new_review(review)
     @review = review
-    mail( from: "admin@api-addicts.herokuapp.com",
-          to: review.api.user.email,
-          subject: "New Review for #{review.api.name}"
-    )
+    mail(from: "admin@api-addicts.herokuapp.com",
+         to: review.api.user.email,
+         subject: "New Review for #{review.api.name}"
+         )
   end
 end
