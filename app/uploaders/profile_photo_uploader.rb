fb1a569db967fbcb865b7b56f25cd7fc1216c50e
Misigno <efournier92@gmail.com>
2016-04-13 22:05:15 -0400

    adds ENV

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -1,5 +1,10 @@
 class ProfilePhotoUploader < CarrierWave::Uploader::Base
-  storage :file
+  if Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
