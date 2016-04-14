8412c64e4ce4f489fa843fe5d899ac04c1afec4c
bmordas11 <brett.mordas@gmail.com>
2016-04-14 10:22:01 -0400

    Changed send-to email to review.api.user.email

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -2,7 +2,7 @@ class ReviewMailer < ApplicationMailer
   def new_review(review)
     @review = review
     mail( from: "admin@api-addicts.herokuapp.com",
-          to: "brett.mordas@gmail.com",
+          to: review.api.user.email,
           subject: "New Review for #{review.api.name}"
     )
   end
