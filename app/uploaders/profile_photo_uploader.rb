931bdbc18a00076db76f53728d68acc2a2d277ca
Misigno <efournier92@gmail.com>
2016-04-13 22:40:40 -0400

    fixes profile_photo_uploader.rb

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -4,7 +4,6 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   else
     storage :fog
   end
-end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
