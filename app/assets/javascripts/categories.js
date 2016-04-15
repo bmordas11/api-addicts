5d409b2097c9268fb79ec0217ff5dfe18f58c350
Matt T <MMT@Matts-MacBook-Pro.local>
2016-04-15 09:24:01 -0400

    cleaning commented code out

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -5,11 +5,6 @@ function init(){
   for (var i = 0; i < categories.length; i++) {
     document.getElementById(categories[i].id).addEventListener("click", changeApis);
   }
-  // $.ajaxSetup({
-  //             'beforeSend': function (xhr){
-  //             xhr.setRequestHeader("Accept", "text/javascript")}
-  // });
-
 }
 
 function changeApis() {
@@ -17,11 +12,9 @@ function changeApis() {
   var request = $.ajax({
     method: "GET",
     url: "/v1/change/" + event.currentTarget.id
-    // data: { api_id: id }
   });
 
   request.done(function(data) {
-    debugger;
     destroyCurrent();
     $('.trending-section').append( "<div class='large-title'>" + category + "</div>" );
     $('.trending-section').append(data)
@@ -31,7 +24,3 @@ function changeApis() {
 function destroyCurrent(){
   $('.trending-section').children().remove();
 }
-
-// function attachNewApi(){
-//   $('.trending-section').append("<%= escape_javascript(render '/apis/index.html.erb' ) %>")
-// }
