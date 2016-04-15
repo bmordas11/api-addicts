912b342e82c08682ceb3c89e13ec057bac904a1f
Misigno <efournier92@gmail.com>
2016-04-15 11:58:16 -0400

    adds pagination

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,8 @@ module Api::V1
   class ApiController < ApplicationController
 
     def change
-      @apis = Api.where(category: params[:category])
+      apis = Api.where(category: params[:category])
+      @apis = apis.paginate(page: params[:page], per_page: 1)
 
       respond_to do |format|
         format.html { render partial: 'change', locals: { apis: @apis }}
