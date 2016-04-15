40f6ae63be2679ad6ffcc613b8c67a4f99cfe981
Matt T <MMT@Matts-MacBook-Pro.local>
2016-04-15 09:29:08 -0400

    cleaning commented code out

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,16 +1,11 @@
 module Api::V1
   class ApiController < ApplicationController
-    # def change
-    # @Apis = Api.where(category: params[:category])
-    # render json: @Apis
 
     def change
       @apis = Api.where(category: params[:category])
 
       respond_to do |format|
         format.html { render partial: 'change', locals: { apis: @apis }}
-        # format.js #auto-maps to preview.js.erb
-        # format.js { render partial: 'change', locals: { apis: @apis }}
       end
     end
   end
